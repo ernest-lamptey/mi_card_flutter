1fbdae61ca211c1bca3580d8ef8af970c068fb92
Ernest <1ernestlamptey@gmail.com>
2023-01-21 23:16:42 +0000

    add profile picture

--- lib/main.dart
+++ lib/main.dart
@@ -13,34 +13,13 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: <Widget>[
-              Container(
-                width: 100.0,
-                height: double.infinity,
-                color: Colors.red,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.green,
-                  ),
-                ]
-              ),
-              Container(
-                width: 100.0,
-                height: double.infinity,
-                color: Colors.blue,
-              ),
+              CircleAvatar(
+                foregroundImage: AssetImage('images/my_pp.png'),
+                radius: 50.0,
+                backgroundColor: Colors.red,
+              )
             ],
           ),
         ),
