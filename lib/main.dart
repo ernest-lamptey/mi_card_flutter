d33e10aa296e8b00b4a8245bf93530f00f4fe77e
Ernest <1ernestlamptey@gmail.com>
2023-01-22 00:37:31 +0000

    finalize card

--- lib/main.dart
+++ lib/main.dart
@@ -12,33 +12,86 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                foregroundImage: AssetImage('images/my_pp.png'),
-                radius: 50.0,
-              ),
-              Text(
-                'ernstblack',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: <Color>[
+                Colors.purple.shade700,
+                Colors.pinkAccent.shade700,
+                Colors.purple.shade700,
+              ]
+            )
+          ),
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  foregroundImage: AssetImage('images/my_pp.png'),
+                  radius: 50.0,
                 ),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  fontFamily: 'SourceSansPro',
-                  fontSize: 20.0,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.teal.shade100,
+                Text(
+                  'ernstblack',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-            ],
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple.shade100,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.purple.shade100,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.purple.shade700,
+                    ),
+                    title: Text('+233 111 222 3333',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.purple.shade700,
+                        fontFamily: 'SourceSansPro',
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.purple.shade700,
+                    ),
+                    title: Text('abc@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.purple.shade700,
+                        fontFamily: 'SourceSansPro',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
